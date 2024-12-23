b3ec3f2e48a27ce5298201993a10ae189ff0c274
Nyein Chan Soe <nyeinchansoe.business@gmail.com>
2024-12-23 23:30:53 +0700

    Standardize Camera class

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -10,13 +10,41 @@
 class Camera
 {
 public:
-    glm::vec3 Position, Front, Up, Right;
-    float Yaw, Pitch, Speed, RotationSpeed, Sensitivity;
+    Camera(glm::vec3 position);
 
-    Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f));
     glm::mat4 GetViewMatrix() const;
-    void ProcessInput(GLFWwindow *window);
+    void ProcessInput();
     void ProcessMouseMovement(float xOffset, float yOffset);
+
+    const inline glm::vec3 &GetPosition() const { return m_Position; }
+    const inline glm::vec3 &GetFront() const { return m_Front; }
+    const inline glm::vec3 &GetUp() const { return m_Up; }
+    const inline glm::vec3 &GetRight() const { return m_Right; }
+    float inline GetPitch() const { return m_Pitch; }
+    float inline GetYaw() const { return m_Yaw; }
+    float inline GetSpeed() const { return m_Speed; }
+    float inline GetRotationSpeed() const { return m_RotationSpeed; }
+    float inline GetSensitivity() const { return m_Sensitivity; }
+
+    void SetPosition(const glm::vec3 &position) { m_Position = position; }
+    void SetFront(const glm::vec3 &front) { m_Front = front; }
+    void SetYaw(float yaw) { m_Yaw = yaw; }
+    void SetPitch(float pitch) { m_Pitch = pitch; }
+    void SetSpeed(float speed) { m_Speed = speed; }
+    void SetRotationSpeed(float rotationSpeed) { m_RotationSpeed = rotationSpeed; }
+    void SetSensitivity(float sensitivity) { m_Sensitivity = sensitivity; }
+
+private:
+    glm::vec3 m_Position;
+    glm::vec3 m_Front;
+    glm::vec3 m_Up;
+    glm::vec3 m_Right;
+
+    float m_Yaw;
+    float m_Pitch;
+    float m_Speed;
+    float m_RotationSpeed;
+    float m_Sensitivity;
 };
 
 #endif
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,61 +1,61 @@
 #include "Camera.hpp"
 #include <glm/gtc/matrix_transform.hpp>
+#include "Application.hpp"
 
 Camera::Camera(glm::vec3 position)
-    : Position(glm::vec3(0.0f, 0.0f, 70.0f)),
-      Front(glm::normalize(glm::vec3(0.0f, 0.0f, 0.0f))),
-      Up(0.0f, 1.0f, 0.0f),
-      Yaw(-50.0f),
-      Pitch(0.0f),
-      Speed(0.2f),
-      RotationSpeed(0.6f),
-      Sensitivity(0.05f)
+    : m_Position(glm::vec3(0.0f, 0.0f, 70.0f)),
+      m_Front(glm::normalize(glm::vec3(0.0f, 0.0f, 0.0f))),
+      m_Up(0.0f, 1.0f, 0.0f),
+      m_Yaw(-50.0f),
+      m_Pitch(0.0f),
+      m_Speed(0.2f),
+      m_RotationSpeed(0.6f),
+      m_Sensitivity(0.05f)
 {
-    // move front
-    // this->Position += 20.0f * glm::normalize(glm::vec3(10.0f, -0.2f, -1.0f));
 }
 
 glm::mat4 Camera::GetViewMatrix() const
 {
-    return glm::lookAt(Position, Position + Front, Up);
+    return glm::lookAt(m_Position, m_Position + m_Front, m_Up);
 }
 
-void Camera::ProcessInput(GLFWwindow *window)
+void Camera::ProcessInput()
 {
-    glm::vec3 Right = glm::normalize(glm::cross(Front, Up)); // Calculate right vector once
+    auto *window = Application::Get().GetWindow().GetNativeWindow();
+    glm::vec3 Right = glm::normalize(glm::cross(m_Front, m_Up)); // Calculate right vector once
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-        Position += Speed * Front;
+        m_Position += m_Speed * m_Front;
     if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        Position -= Speed * Front;
+        m_Position -= m_Speed * m_Front;
     if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-        Position -= Speed * Right;
+        m_Position -= m_Speed * Right;
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-        Position += Speed * Right;
+        m_Position += m_Speed * Right;
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
-        Position += Speed * Up;
+        m_Position += m_Speed * m_Up;
     if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
-        Position -= Speed * Up;
+        m_Position -= m_Speed * m_Up;
     if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
-        Yaw -= RotationSpeed;
+        m_Yaw -= m_RotationSpeed;
     if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-        Yaw += RotationSpeed;
+        m_Yaw += m_RotationSpeed;
 
     glm::vec3 front;
-    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-    front.y = sin(glm::radians(Pitch));
-    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-    Front = glm::normalize(front);
+    front.x = cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+    front.y = sin(glm::radians(m_Pitch));
+    front.z = sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch));
+    m_Front = glm::normalize(front);
 }
 
 void Camera::ProcessMouseMovement(float xOffset, float yOffset)
 {
-    xOffset *= Sensitivity;
-    yOffset *= Sensitivity;
-    Yaw += xOffset;
-    Pitch = glm::clamp(Pitch + yOffset, -89.0f, 89.0f);
-    Front = glm::normalize(glm::vec3(
-        cos(glm::radians(Yaw)) * cos(glm::radians(Pitch)),
-        sin(glm::radians(Pitch)),
-        sin(glm::radians(Yaw)) * cos(glm::radians(Pitch))));
+    xOffset *= m_Sensitivity;
+    yOffset *= m_Sensitivity;
+    m_Yaw += xOffset;
+    m_Pitch = glm::clamp(m_Pitch + yOffset, -89.0f, 89.0f);
+    m_Front = glm::normalize(glm::vec3(
+        cos(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch)),
+        sin(glm::radians(m_Pitch)),
+        sin(glm::radians(m_Yaw)) * cos(glm::radians(m_Pitch))));
 }
